03a84da337c70ab1f26406b3824434a806cbec3b
Anar <anar.rzayev94@gmail.com>
2021-01-25 00:06:52 +0400

    Update binary_tree.py

--- binary_tree.py
+++ binary_tree.py
@@ -35,7 +35,7 @@ class Node:
 
     def PrintTree(self):
         if self.left:
-            print("im left")
+            print("im left not left")
             self.left.PrintTree()
 
         print("data", self.data)
